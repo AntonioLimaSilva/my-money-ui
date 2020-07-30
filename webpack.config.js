22657de2118bf47df8a655ba4b240b181003cba4
Luciano Lima <luclimasilva@hotmail.com>
2020-07-30 17:14:22 -0300

    ajuste

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,11 @@ module.exports = {
         }
     },
     plugins: [ 
+        new webpack.DefinePlugin({
+            'process.env': {
+              'NODE_ENV': JSON.stringify('production')
+            }
+        }),
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
